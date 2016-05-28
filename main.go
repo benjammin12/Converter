62aed54bb881469fe4ea255ed50fc672902a1644
benjammin12 <benxerri@gmail.com>
2016-05-28 19:37:04 -0400

    Added divider

--- main.go
+++ main.go
@@ -6,6 +6,7 @@ func main() {
 	var (
 		userInput int //choice for value you'd like to converted to
 		converted int //hold number you would like to convert
+		divider   = "---------------------"
 	)
 
 	fmt.Println("What would you like to convert?") //print out choices
@@ -19,15 +20,21 @@ func main() {
 	case 1: //user picked one, for Binary
 		fmt.Println("Binary conversion, Enter your number: ")
 		fmt.Scanln(&converted) //store value the user enters
-		fmt.Printf("Your converted binary number is %b\n", converted)
+		fmt.Println("Your number:", converted)
+		fmt.Println(divider)
+		fmt.Printf("Binary number: %b\n\n", converted)
 	case 2: //user picked two, for Hexi
 		fmt.Println("Hexidecimal conversion,  Enter your number: ")
 		fmt.Scanln(&converted)
-		fmt.Printf("Your converted Hexidecimal number is %X\n", converted)
+		fmt.Println("Your number:", converted)
+		fmt.Println(divider)
+		fmt.Printf("Hexidecimal number: %X\n\n", converted)
 	case 3: //user picked three, for unicode
 		fmt.Println("Unicode Conversion, Enter your number: ")
 		fmt.Scanln(&converted)
-		fmt.Printf("Your converted Unicode number is %U\n", converted)
+		fmt.Println("Your number:", converted)
+		fmt.Println(divider)
+		fmt.Printf("Unicode number: %U\n\n", converted)
 	default: //you didn't pick one through thee
 		fmt.Println("not a known input")
 
