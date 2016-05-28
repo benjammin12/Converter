7ac3fe0b93b8499a28157db1a97576fc459197ae
benjammin12 <benxerri@gmail.com>
2016-05-28 18:25:12 -0400

    Added unicode conversion and comments

--- main.go
+++ main.go
@@ -3,20 +3,33 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("What would you like to convert?")
-  fmt.Println("1: Binary")
-  fmt.Println("2: Hex")
-  var user_input int
-  var converted int
+	var (
+		userInput int //choice for value you'd like to converted to
+		converted int //hold number you would like to convert
+	)
 
-  fmt.Scanln(&user_input)
+	fmt.Println("What would you like to convert?") //print out choices
+	fmt.Println("1: Binary")
+	fmt.Println("2: Hex")
+	fmt.Println("3: Unicode")
 
-    switch user_input {
-  case 1: fmt.Println("Binary conversion, Enter your number: ")
-          fmt.Scanln(&converted)
-          fmt.Printf("Your converted binary number is %b\n" , converted)
-  case 2: fmt.Printf("%x", &user_input)
-  default: fmt.Println("not a known input")
+	fmt.Scanln(&userInput)
 
-  }
+	switch userInput {
+	case 1: //user picked one, for Binary
+		fmt.Println("Binary conversion, Enter your number: ")
+		fmt.Scanln(&converted) //store value the user enters
+		fmt.Printf("Your converted binary number is %b\n", converted)
+	case 2: //user picked two, for Hexi
+		fmt.Println("Hexidecimal conversion,  Enter your number: ")
+		fmt.Scanln(&converted)
+		fmt.Printf("Your converted Hexidecimal number is %X\n", converted)
+	case 3: //user picked three, for unicode
+		fmt.Println("Unicode Conversion, Enter your number: ")
+		fmt.Scanln(&converted)
+		fmt.Printf("Your converted Unicode number is %U\n", converted)
+	default: //you didn't pick one through thee
+		fmt.Println("not a known input")
+
+	}
 }
