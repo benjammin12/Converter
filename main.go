d7e2be95fa87be6e951f6fd0597806ceb2e04198
benjammin12 <benxerri@gmail.com>
2016-05-27 23:10:10 -0400

    Updated case 1

--- main.go
+++ main.go
@@ -4,13 +4,18 @@ import "fmt"
 
 func main() {
   fmt.Println("What would you like to convert?")
-  var user_input string
+  fmt.Println("1: Binary")
+  fmt.Println("2: Hex")
+  var user_input int
+  var converted int
 
-  fmt.Scanf("%s", &user_input)
+  fmt.Scanln(&user_input)
 
     switch user_input {
-  case "binary": fmt.Println("Binary conversion, Enter your number: ")
-  case "hex": fmt.Printf("%x", &user_input)
+  case 1: fmt.Println("Binary conversion, Enter your number: ")
+          fmt.Scanln(&converted)
+          fmt.Printf("Your converted binary number is %b\n" , converted)
+  case 2: fmt.Printf("%x", &user_input)
   default: fmt.Println("not a known input")
 
   }
